e97424f416ac4b3c4b704e9d28bcc319f2f5fa7f
Szymon Wiergowski <s.wiergowski@gmail.com>
2020-05-06 23:57:33 +0200

    fix footer

--- gatsby-site/src/pages/index.js
+++ gatsby-site/src/pages/index.js
@@ -10,24 +10,31 @@ const IndexPage = () => (
     <div>
       <Image />
     </div>
-    <h1>O firmie</h1>
-    <p>
-      Kancelaria Majątkowa została założona w 2017 r. przez rzeczoznawcę
-      majątkowego Tomasza Rzóska, posiadającego uprawnienia zawodowe numer 7159
-      w zakresie szacowania nieruchomości.
-    </p>
-    <p>
-      Świadczymy usługi w zakresie wyceny nieruchomości oraz doradztwa
-      majątkowego, skierowane do klientów indywidualnych, przedsiębiorstw oraz
-      instytucji z sektora finansowego i publicznego. Zapewniamy profesjonalną i
-      kompleksową obsługę klienta, terminowość oraz rzetelne wykonanie zlecenia,
-      zgodnie z obowiązującymi przepisami prawa i standardami zawodowymi
-      rzeczoznawców majątkowych.
-    </p>
-    <p>
-      Właściciel firmy jest członkiem Pomorskiego Towarzystwa Rzeczoznawców
-      Majątkowych.
-    </p>
+    <div
+      style={{
+        margin: `2rem auto`,
+        maxWidth: 1200,
+      }}
+    >
+      <h1>O firmie</h1>
+      <p>
+        Kancelaria Majątkowa została założona w 2017 r. przez rzeczoznawcę
+        majątkowego Tomasza Rzóska, posiadającego uprawnienia zawodowe numer
+        7159 w zakresie szacowania nieruchomości.
+      </p>
+      <p>
+        Świadczymy usługi w zakresie wyceny nieruchomości oraz doradztwa
+        majątkowego, skierowane do klientów indywidualnych, przedsiębiorstw oraz
+        instytucji z finansowego i publicznego. Zapewniamy profesjonalną i
+        kompleksową obsługę klienta, terminowość oraz rzetelne wykonanie
+        zlecenia, zgodnie z obowiązującymi przepisami prawa i standardami
+        zawodowymi rzeczoznawców majątkowych.
+      </p>
+      <p>
+        Właściciel firmy jest członkiem Pomorskiego Towarzystwa Rzeczoznawców
+        Majątkowych.
+      </p>
+    </div>
   </Layout>
 )
 
