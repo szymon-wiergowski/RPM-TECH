3045e0c379947a30d0f0cdd2827a6184db97be3c
Szymon Wiergowski <s.wiergowski@gmail.com>
2020-05-14 22:33:39 +0200

    add Draver to navbar

--- gatsby-site/src/components/header.js
+++ gatsby-site/src/components/header.js
@@ -1,42 +1,40 @@
-import { Link } from "gatsby";
-import React, { useState } from "react";
+import { Link } from "gatsby"
+import React from "react"
 
-import styles from "./header.module.css";
-
-import logo from "../images/logo-rpm-tech.svg";
+import styles from "./header.module.css"
+import logo from "../images/logo-rpm-tech.svg"
+import Drawer from "./drawer"
 
 const Header = ({ siteTitle }) => {
-  const [active, setActive] = useState(false)
-  const [headerActiveClass, setHeaderActiveClass] = useState("")
-
-  const toggleActiveClass = (event, newValue) => {
-    setActive(!active)
-    console.log(active)
+  let width = window.innerWidth
+  if (width < 768) {
+    return (<Drawer />)
+  } else {
+    return (
+      <header>
+        <div className={styles.container}>
+          <div className={styles.brand}>
+            <img src={logo} alt="logo" className={styles.brand_logo} />
+            <h1 className={styles.brand_name}>{siteTitle}</h1>
+          </div>
+          <div className={styles.links} >
+            <Link  className={styles.links_item} to="/">
+              <h4>O NAS</h4>
+            </Link>
+            <Link className={styles.links_item} to="/services">
+              <h4>USŁUGI</h4>
+            </Link>
+            <Link className={styles.links_item} to="/projects">
+              <h4>REALIZACJE</h4>
+            </Link>
+            <Link className={styles.links_item} to="/contact">
+              <h4>KONTAKT</h4>
+            </Link>
+          </div>
+        </div>
+      </header>
+    )
   }
-  return (
-    <header>
-      <div className={styles.container}>
-        <div className={styles.brand}>
-          <img src={logo} alt="logo" className={styles.brand_logo} />
-          <h1 className={styles.brand_name}>{siteTitle}</h1>
-        </div>
-        <div className={styles.links} onClick={() => toggleActiveClass()}>
-          <Link value={active} className={styles.links_item} to="/">
-            <h4>O NAS</h4>
-          </Link>
-          <Link value={active} className={styles.links_item} to="/services">
-            <h4>USŁUGI</h4>
-          </Link>
-          <Link value={active} className={styles.links_item} to="/projects">
-            <h4>REALIZACJE</h4>
-          </Link>
-          <Link value={active} className={styles.links_item} to="/contact">
-            <h4>KONTAKT</h4>
-          </Link>
-        </div>
-      </div>
-    </header>
-  )
 }
 
 export default Header
