82cb2ab2ef0829723d34a841b324b66794817fd4
Szymon Wiergowski <s.wiergowski@gmail.com>
2020-05-15 21:27:22 +0200

    fix image.js

--- gatsby-site/src/pages/index.js
+++ gatsby-site/src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 
 import Layout from "../components/layout"
-import Image from "../components/about"
+import Image from "../components/image"
 import SEO from "../components/seo"
 import styles from "./index.module.css"
 
